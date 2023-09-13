6d1a8063266ce1e71fe20a00b10c1ce786d8b4fc
Gibi <gibialexmathew@gmail.com>
2023-09-13 17:17:33 +0530

    commit

--- Test Suites/Login and book an appointment.ts
+++ Test Suites/Login and book an appointment.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Smoke test</name>
+   <name>Login and book an appointment</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,23 +10,23 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>8a8a1347-909b-4e3f-8428-ae93819c0219</testSuiteGuid>
+   <testSuiteGuid>d2c7cb10-b9c5-46c0-ad22-e8a313cc29f6</testSuiteGuid>
    <testCaseLink>
-      <guid>40feb4e0-cb07-4dc2-823e-e5333a2de466</guid>
+      <guid>979d8fe8-ec0c-4b1c-9f39-9858e1117706</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Reuse test cases/RT001_Login for appointment</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e7f8ebc9-d09e-4a65-ae68-db3505a21eab</guid>
+      <guid>56403e3b-21d4-4ea7-a45e-5b49bf47bdae</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Reuse test cases/RT002_Book an appointment</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>4fe91895-03a6-4de8-aa26-3abb698f5692</guid>
+      <guid>0b080675-5154-4ad6-9feb-5902fef04226</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Reuse test cases/RT003_Logout of the application</testCaseId>
